4afdb4e621e05333315f673d5987d9918838ca09
Elvis <me@elvisw.com>
2022-09-19 23:19:08 +0800

    Ch8 done

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using System.Linq;
 
-namespace SportsStore.Pages
-{
-    public class CartModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace SportsStore.Pages {
+
+    public class CartModel : PageModel {
+        private readonly IStoreRepository _repository;
+
+        public CartModel(IStoreRepository repo) {
+            _repository = repo;
+        }
+
+        public Cart Cart { get; set; }
+        public string? ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl) {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnUrl) {
+            Product? product = _repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
+
 }
